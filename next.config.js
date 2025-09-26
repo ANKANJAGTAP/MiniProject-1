22ea6c37c63c010f7ef106ddd066d8296faa8721
ANKANJAGTAP <157878894+ANKANJAGTAP@users.noreply.github.com>
2025-09-26 10:43:16 +0530

    Fix Supabase environment variables configuration

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-  },
 };
 
 module.exports = nextConfig;
